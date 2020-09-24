8386d4ffe34a044cb0711e17d203a75d502bd063
Jalal azimi <jalalazimi@gmail.com>
2020-09-24 22:31:42 +0100

    spinner percentage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import "./dclick";
-import "./users";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
